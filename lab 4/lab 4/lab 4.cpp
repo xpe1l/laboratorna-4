fc185f641154a4ee57d0edf67e8f9d9c171d7e96
xpe1l <semenuk.boghdan@gmail.com>
2023-01-04 00:57:06 +0200

    lab 4

---
+++ lab 4/lab 4/lab 4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int num{};
+    cout << "enter a number"<< endl;
+    cin >> num;
+
+    for (int i = 0; i < num; i++) {
+        for (int j = 0; j < num - i; j++) {
+            cout << " ";
+        }
+        for (int r = 0; r < i; r++) {
+            cout << "#";
+        }
+        for (int l = 0; l < i + 1; l++) {
+            cout << "#";
+        }
+        cout << "\n";
+
+
+    }
+}
